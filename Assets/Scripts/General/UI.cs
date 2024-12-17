21ae2b873fdf8bf49f13209932827f77375c90b7
Jacques-DuToit <jacques_dutoit@outlook.com>
2024-12-17 20:48:41 +0000

    feat: save and load

--- Assets/Scripts/General/UI.cs
+++ Assets/Scripts/General/UI.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,7 +20,74 @@ public class UI : MonoBehaviour, IInputReceiver
 
     [SerializeField] GameObject choiceButtonsLayout;
     [SerializeField] GameObject generalChoiceButton;
-    GameObject thisChoiceButton;
+
+    private string ListToString(List<string> list)
+    {
+        // Efficiently concatenates strings using StringBuilder
+        StringBuilder sb = new StringBuilder();
+
+        foreach (string item in list)
+        {
+            // Append the length, a separator, and the item itself
+            sb.Append(item.Length);
+            sb.Append('_');
+            sb.Append(item);
+        }
+        return sb.ToString();
+    }
+
+    private List<string> StringToList(string str)
+    {
+        // Turns an encoded string back into its original list
+        List<string> list = new List<string>();
+
+        int i = 0;
+        string itemLength = "";
+        string nextItem;
+
+        while (i < str.Length)
+        {
+            if (str[i] != '_')
+            {
+                // Keep grabbing the next number until we hit a _
+                itemLength += str[i].ToString();
+                i++;
+            }
+            else
+            {
+                // When we hit a _, grab the next item and add it to the list
+                i++; // Skip over the _
+                nextItem = str.Substring(i, int.Parse(itemLength));
+                print(nextItem);
+                list.Add(nextItem);
+                // Skip to next number and reset itemLength
+                i += int.Parse(itemLength);
+                itemLength = "";
+            }
+        }
+        return list;
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt("Voters", voters);
+        PlayerPrefs.SetString("Choices", ListToString(choices));
+    }
+
+    public void LoadData()
+    {
+        HandleVoterInput(PlayerPrefs.GetInt("Voters").ToString());
+        choices = StringToList(PlayerPrefs.GetString("Choices"));
+
+        foreach (string choice in choices)
+        {
+            CreateChoiceButton(choice);
+        }
+
+        print("Here");
+        print(voters);
+        print(ListToString(choices));
+    }
 
     private void Start()
     {
@@ -27,6 +95,8 @@ public class UI : MonoBehaviour, IInputReceiver
         {
             Debug.LogError("Either voters or choices fields not assigned in inspector.");
         }
+      
+        LoadData();
 
         inputFieldActions = new Dictionary<string, Action<string>>
         {
@@ -62,21 +132,31 @@ public class UI : MonoBehaviour, IInputReceiver
         SwitchInput();
     }
 
+    private void CreateChoiceButton(string choiceInput)
+    {
+        GameObject thisChoiceButton = Instantiate(generalChoiceButton, choiceButtonsLayout.transform);
+        thisChoiceButton.GetComponent<ChoiceButton>().AddText(choiceInput);
+    }
+
     private void HandleChoicesInput(string choiceInput)
     {
-        if (choiceInput != "" && !choices.Contains(choiceInput))
+        if (choiceInput == "" || choices.Contains(choiceInput))
         {
-            // Add to list of current choices
-            choices.Add(choiceInput);
-            // Add the choice to the layout so the user can see and remove it
-            thisChoiceButton = Instantiate(generalChoiceButton, choiceButtonsLayout.transform);
-            thisChoiceButton.GetComponent<ChoiceButton>().AddText(choiceInput);
+            return;
         }
+        // Add to list of current choices
+        choices.Add(choiceInput);
+        // Add the choice to the layout so the user can see and remove it
+        CreateChoiceButton(choiceInput);
+        SaveData();
     }
 
     public void RemoveChoice(string choice)
     {
+        print(ListToString(choices));
+        print( StringToList(ListToString(choices)) );
         choices.Remove(choice);
+        SaveData();
     }
 
     public void SwitchInput()
