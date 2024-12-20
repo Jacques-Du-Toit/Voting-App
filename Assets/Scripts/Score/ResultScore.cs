a53679baa6db00a10263b408113e8b6faab1dcfa
Jacques-DuToit <jacques_dutoit@outlook.com>
2024-12-20 11:26:30 +0000

    refactor: move table code to table object

--- Assets/Scripts/Score/ResultScore.cs
+++ Assets/Scripts/Score/ResultScore.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using System.Linq;
 
 public class ResultScore : MonoBehaviour
@@ -19,71 +18,6 @@ public class ResultScore : MonoBehaviour
             { "choice 3", new int[] { -4, 4, -5 } },
         };
         PerChoiceResults(scores);
-    }
-
-    float[] InitialiseArray(int size, float value)
-    {
-        float[] array = new float[size];
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = value;
-        }
-        return array;
-    }
-
-    void ColorTable(GameObject table)
-    {
-        Transform tablePos = table.transform;
-        Transform row;
-        Cell cell;
-        float cellVal;
-        Image cellCol;
-        // Stores the best and worst values per column index
-        float[] bestValues = InitialiseArray(4, -5f);
-        float[] worstValues = InitialiseArray(4, 5f);
-
-        Color softGreen = new Color(0.3f, 0.8f, 0.3f);
-        Color softRed = new Color(0.8f, 0.3f, 0.3f);
-
-
-        // First pass to get the best and worst values
-        for (int row_i = 1; row_i < tablePos.childCount; row_i++) // skip first row as header
-        {
-            row = tablePos.GetChild(row_i);
-            for (int col_i = 1; col_i < row.childCount; col_i++) // skip first col as index
-            {
-                cell = row.GetChild(col_i).GetComponent<Cell>();
-                cellVal = float.Parse(cell.cellValue.text);
-                if (cellVal > bestValues[col_i - 1])
-                {
-                    bestValues[col_i - 1] = cellVal;
-                }
-                if (cellVal < worstValues[col_i - 1])
-                {
-                    worstValues[col_i - 1] = cellVal;
-                }
-            }
-        }
-
-        // Second pass to change colors of the best and worst values
-        for (int row_i = 1; row_i < tablePos.childCount; row_i++)
-        {
-            row = tablePos.GetChild(row_i);
-            for (int col_i = 1; col_i < row.childCount; col_i++)
-            {
-                cell = row.GetChild(col_i).GetComponent<Cell>();
-                cellVal = float.Parse(cell.cellValue.text);
-                cellCol = cell.cellColor;
-                if (cellVal == bestValues[col_i - 1])
-                {
-                    cellCol.color = softGreen;
-                }
-                else if (cellVal == worstValues[col_i - 1])
-                {
-                    cellCol.color = softRed;
-                }
-            }
-        }
     }
 
     void PerChoiceResults(Dictionary<string, int[]> scores)
@@ -124,6 +58,6 @@ public class ResultScore : MonoBehaviour
             rowStats.GetComponent<Row>().AddValues(c, avg, std, min, max);
             c++;
         }
-        ColorTable(choiceStats);
+        choiceStats.GetComponent<Table>().ColorTable();
     }
 }
