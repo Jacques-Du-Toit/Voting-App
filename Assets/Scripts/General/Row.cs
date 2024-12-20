51dd67f05a238608a920a0852235b32b01cabd53
Jacques-DuToit <jacques_dutoit@outlook.com>
2024-12-20 11:22:45 +0000

    feat: table colors

--- Assets/Scripts/General/Row.cs
+++ Assets/Scripts/General/Row.cs
@@ -5,15 +5,6 @@ public class Row : MonoBehaviour
     public void AddValues(params float[] values)
     {
         Transform thisPos = this.transform;
-        Debug.Log($"Child count of {thisPos.name}");
-        Debug.Log($"{thisPos.childCount}");
-
-        foreach (var value in values)
-        {
-            print(value);
-        }
-
-        print(thisPos.childCount);
 
         for (int i = 0; i < thisPos.childCount; i++)
         {
