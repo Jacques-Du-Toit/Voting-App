02776618c5a845e215058ea6f47f15fd32bcd2d3
Jacques-DuToit <jacques_dutoit@outlook.com>
2025-02-23 23:27:23 +0000

    docs: better documentation on scoring count functions

--- Assets/Scripts/Score/UIScore.cs
+++ Assets/Scripts/Score/UIScore.cs
@@ -61,6 +61,10 @@ public class UIScore : MonoBehaviour
 
     Dictionary<string, int[]> CountChoiceScores()
     {
+        // Returns a dictionary of this form
+        // { "choiceOne": { firstVoterScore, ..., lastVoterScore }, "choiceTwo": { firstVoterScore, ..., lastVoterScore } }
+        // -> { "movie": { 3, -1, 0 }, "game": { 1, 0, 4 } }
+
         Dictionary<string, int[]> choiceScores = new Dictionary<string, int[]>();
         Dictionary<string, int> systemScores = new Dictionary<string, int>();
         int score;
@@ -90,18 +94,23 @@ public class UIScore : MonoBehaviour
 
     Dictionary<string, int[]> CountVoterScores()
     {
+        // Returns a dictionary of this form
+        // { "voterOne": { firstChoiceScore, ..., lastChoiceScore }, "voterTwo": { firstChoiceScore, ..., lastChoiceScore } }
+        // -> { "1": { 3, 1 }, "2": { -1, 0 }, "3": { 0, 4 } }
+        // (In a sense a transpose of CountChoiceScores)
+
         Dictionary<string, int[]> voterScores = new Dictionary<string, int[]>();
         Dictionary<string, int> systemScores = new Dictionary<string, int>();
 
-        for(int v = 1; v <= voters; v++)
+        for(int v = 0; v < voters; v++)
         {
-            voterScores[v.ToString()] = new int[choices.Count];
+            voterScores[(v + 1).ToString()] = new int[choices.Count];
         }
 
-        for (int i = 0; i < voters; i++)
+        for (int v = 0; v < voters; v++)
         {
-            systemScores = systems[i].GetComponent<ScoreSystem>().choiceScores;
-            voterScores[(i + 1).ToString()] = systemScores.Values.ToArray();
+            systemScores = systems[v].GetComponent<ScoreSystem>().choiceScores;
+            voterScores[(v + 1).ToString()] = systemScores.Values.ToArray();
         }
         return voterScores;
     }
