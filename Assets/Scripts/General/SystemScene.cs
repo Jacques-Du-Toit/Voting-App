34db95744a2fcd4d5ada9cb5f4704d65238a3dbf
Jacques-DuToit <jacques_dutoit@outlook.com>
2025-03-01 17:10:41 +0000

    feat: shuffle choices

--- Assets/Scripts/General/SystemScene.cs
+++ Assets/Scripts/General/SystemScene.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class SystemScene : MonoBehaviour
 {
--- Assets/Scripts/Rank/RankSystem.cs
+++ Assets/Scripts/Rank/RankSystem.cs
@@ -9,7 +9,7 @@ public class RankSystem : MonoBehaviour, IVoteSystem
 
     [SerializeField] GameObject content;
     [SerializeField] GameObject rankChoice;
-
+    Transform parentPos;
     Dictionary<string, RankChoice> choiceScripts;
     public Dictionary<string, int> ChoiceVotes { get; private set; }
 
@@ -26,7 +26,7 @@ public class RankSystem : MonoBehaviour, IVoteSystem
     {
         voters = Data.voters;
         choices = Data.choices;
-
+        parentPos = content.transform;
         InitialiseChoices();
     }
 
@@ -39,6 +39,9 @@ public class RankSystem : MonoBehaviour, IVoteSystem
 
         GameObject thisChoice;
         RankChoice choiceScript;
+
+        choices.Shuffle();
+        int i = choices.Count - 1; // voters are added top to bottom so top gets highest score
         foreach (string choice in choices)
         {
             // Instantiate the GameObject
@@ -48,8 +51,41 @@ public class RankSystem : MonoBehaviour, IVoteSystem
             // Set the text of the ChoiceParent component
             choiceScript.choiceText.text = choice;
 
+            // Set the current rank of the choice in the ChoiceVotes dict
+            ChoiceVotes[choiceScript.choiceText.text] = i;
+            i--;
+
             // Store the instantiated object in the dictionary
             choiceScripts[choice] = choiceScript;
         }
+
+        foreach (var entry in ChoiceVotes)
+        {
+            print($"{entry.Key}: {entry.Value}");
+        }
+    }
+
+    void MoveChoice(string choice, int direction)
+    {
+
+    }
+
+    void CalculateOrder(string choice, int score)
+    {
+        Transform thisPos = choiceScripts[choice].gameObject.transform;
+
+        for (int i = 0; i < parentPos.childCount; i++)
+        {
+            Transform childPos = parentPos.GetChild(i);
+            int childScore = childPos.GetComponent<ChoiceParent>().score;
+
+            if (childPos == thisPos || score < childScore) { continue; }
+            else
+            {
+                thisPos.SetSiblingIndex(i - 1);
+                return;
+            }
+        }
+        thisPos.SetAsLastSibling();
     }
 }
