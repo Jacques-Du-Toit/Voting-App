2a77312c533d1887cf9816ee75769277aeed949b
Jacques-DuToit <jacques_dutoit@outlook.com>
2025-03-05 21:53:40 +0000

    feat: now display first 3 letters of choice so no mapping

--- Assets/Scripts/General/Results.cs
+++ Assets/Scripts/General/Results.cs
@@ -70,7 +70,8 @@ public class Results : MonoBehaviour
                 {
                     bestAvg = avg;
                 }
-                if (min > bestMin) { 
+                if (min > bestMin)
+                {
                     bestMin = min;
                 }
 
@@ -117,38 +118,28 @@ public class Results : MonoBehaviour
             title = title.TrimEnd(' ', ',');
             title += "</size>\n";
 
-            // Maps the strings to an index for the table as full strings are too big
-            List<string> keys = new List<string>(indexValues.Keys);
-            title += "<size=30>";
-            for (int i = 0; i < keys.Count; i++)
+            thisTableStats.GetComponent<Table>().SetTitle(title);
+
+            foreach (var entry in indexValues)
             {
-                title += $"{i + 1}: {keys[i]}, ";
+                // Add a new row to the table
+                rowStats = Instantiate(statsRow, thisTableStats.transform);
+
+                // Calculate stats
+                avg = Mathf.Round((float)entry.Value.Average() * 100f) / 100f;
+                sumOfSquares = entry.Value.Select(val => Mathf.Pow(val - avg, 2)).Sum();
+                std = Mathf.Round(Mathf.Sqrt(sumOfSquares / entry.Value.Length) * 100f) / 100f;
+                min = entry.Value.Min();
+                max = entry.Value.Max();
+
+                // Send stats to the row so it can add them
+                rowStats.GetComponent<Row>().AddValues(
+                    (string)entry.Key.Substring(0, 3), avg.ToString(), std.ToString(), min.ToString(), max.ToString()
+                    );
             }
-            title = title.TrimEnd(' ', ',');
-            title += "\n</size>";
+            thisTableStats.GetComponent<Table>().ColorTable();
         }
-        thisTableStats.GetComponent<Table>().SetTitle(title);
-
-        int c = 1;
-        foreach (var entry in indexValues)
-        {
-            // Add a new row to the table
-            rowStats = Instantiate(statsRow, thisTableStats.transform);
-
-            // Calculate stats
-            avg = Mathf.Round((float)entry.Value.Average() * 100f) / 100f;
-            sumOfSquares = entry.Value.Select(val => Mathf.Pow(val - avg, 2)).Sum();
-            std = Mathf.Round(Mathf.Sqrt(sumOfSquares / entry.Value.Length) * 100f) / 100f;
-            min = entry.Value.Min();
-            max = entry.Value.Max();
-
-            // Send stats to the row so it can add them
-            rowStats.GetComponent<Row>().AddValues(c, avg, std, min, max);
-            c++;
-        }
-        thisTableStats.GetComponent<Table>().ColorTable();
     }
-
     void VoterSimilarity(Dictionary<string, int[]> voterVotes)
     {
         GameObject thisVoteDiff;
--- Assets/Scripts/General/Table/Row.cs
+++ Assets/Scripts/General/Table/Row.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class Row : MonoBehaviour
 {    
-    public void AddValues(params float[] values)
+    public void AddValues(params string[] values)
     {
         Transform thisPos = this.transform;
 
         for (int i = 0; i < thisPos.childCount; i++)
         {
             Transform childPos = thisPos.GetChild(i);
-            childPos.GetComponent<Cell>().cellValue.text = values[i].ToString();
+            childPos.GetComponent<Cell>().cellValue.text = values[i];
         }
     }
 }
